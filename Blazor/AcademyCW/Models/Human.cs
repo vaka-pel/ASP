c66b938d214c8c1812676076e6bbb7afce59fd9b
Pelivan <Pelivan@DESKTOP-29KL5UV>
2026-09-16 20:27:26 +0300

    TODO: Scafold Student

---
+++ Blazor/AcademyCW/Models/Human.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace AcademyCW.Models
+{
+	public class Human
+	{
+		[Required]
+		[StringLength(50, MinimumLength = 2)]
+		[RegularExpression("^[A-ZА-Я] [a-zа-я]+$")]
+		public string last_name {  get; set; }
+
+		[Required]
+		[StringLength (50, MinimumLength = 2)]
+		public string first_name { get; set; }
+
+		public string? middle_name { get; set; }
+
+		[Required]
+		[DataType(DataType.Date)]
+		public DateOnly birth_date { get; set; }
+		[EmailAddress]
+		public string? email { get; set; }
+		public string? phone { get; set; }
+		public byte[]? photo { get; set; }
+
+	}
+}
---
+++ Blazor/AcademyCW/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace AcademyCW.Models
+{
+	public class Student
+	{
+		[Key]
+		public int stud_id { get; set; }
+		[Required]
+		public int group {  get; set; }
+
+	}
+}
